2428775163c5826c96e4a3ae95ad1c5ec4447665
Pedro Henrique Paulek <pedroxhp@outlook.com>
2018-12-16 15:24:27 -0200

    commit-161218_01[AuditUpdate]

--- src/pages/lista-audit/lista-audit.ts
+++ src/pages/lista-audit/lista-audit.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Audit } from '../../models/audit';
 
 /**
  * Generated class for the ListaAuditPage page.
